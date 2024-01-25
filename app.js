d6d575db0c4263e5fb47a5f99545e50c8d79ceae
Vukosi <vukosimohlabini16@gmail.com>
2024-01-25 13:29:23 +0200

    Almost finished v1
    Added full drawing functionality.
    Added pen colour selection functionality.
    Added box number selection funcionality.

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('.button');
 
-for (i = 1; i <= 16; i++) {
-    const square = document.createElement('div');
-    square.textContent = '#';
-    square.setAttribute('class', 'squares');
-    container.appendChild(square);
+button.addEventListener('click', function() {
+    const userInput = prompt("Enter number of squares in a line you want: ");
+    gridNumber = parseInt(userInput); 
 
-    square.onmouseover = function() {square.style.backgroundColor = "red"; };
-    square.onmouseout = () => square.style.backgroundColor = "";
-}
+    const gridSize = gridNumber * gridNumber;
+
+    const sizeCalculate = 6.5 * gridNumber;
+
+    container.style.width = `${sizeCalculate}px`;
+    container.style.height = `${sizeCalculate}px`;
+
+    const color = prompt("Enter your pen color. Red, Green or Blue. Cancel for black: ").toLowerCase();
+    console.log(color)
+
+    let pen;
+
+    if (color == "red") {
+        pen = "red";
+    } else if (color == "green") {
+        pen = "green"
+    } else if (color == "blue") {
+        pen = "blue"
+    } else {
+        pen = "black";
+    }
+
+
+
+    for (i = 1; i <= gridSize; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'squares');
+        container.appendChild(square);
+
+        square.onmouseover = function() {square.style.backgroundColor = `${pen}`; };
+        buttonClear.addEventListener('click', () => square.style.backgroundColor = 'white');
+    }
+
+    
+}, { once: true });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
